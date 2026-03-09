68d0abaad3574de7288330274eabcc86d02c82c5
prachitalokar363 <prachitalokar363@gmail.com>
2026-03-10 01:02:27 +0530

    ic

---
+++ Day-2-Terraform-all-configuration-files/output.tf
@@ -0,0 +1,9 @@
+output "public_ip" {
+    value = aws_instance.name.public_ip
+}
+output "privateip" {
+    value = aws_instance.name.private_ip
+}
+output "az" {
+    value = aws_instance.name.availability_zone
+}
---
+++ Day-2-Terraform-all-configuration-files/variables.tf
@@ -0,0 +1,10 @@
+variable "ami_id" {
+    default = ""
+    type = string
+  
+}
+variable "instance_type" {
+    default = ""
+    type = string
+  
+}
